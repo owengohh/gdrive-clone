d2a9c1f0a4431315e97194fd89f95e20b833dff3
Owen Goh <owengoh2001@gmail.com>
2025-02-02 14:13:30 +0800

    mock ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import GoogleDriveClone from '../GoogleDriveClone'
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-900">
+        <p className="text-white">Hello World</p>
       <GoogleDriveClone />
     </main>
   )
