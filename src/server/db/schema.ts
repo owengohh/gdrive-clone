3f73c5b0df93ee68527aa0e6b79a1c797deb4816
Owen Goh <owengoh2001@gmail.com>
2025-02-02 21:26:29 +0800

    update schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,27 +1,49 @@
-import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import {
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  serial,
+  index,
+} from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable('users_table', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  age: integer('age').notNull(),
-  email: text('email').notNull().unique(),
-});
+export const filesTable = pgTable(
+  "files",
+  {
+    id: serial("id").primaryKey().notNull(),
+    name: text("name").notNull(),
+    type: text("type").notNull(),
+    url: text("url").notNull(),
+    parentId: integer("parentId").notNull(),
+    size: integer("size").notNull(),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+  },
+  (table) => {
+    return {
+      indexes: [index("parentIdIndex").on(table.parentId)],
+    };
+  },
+);
 
-export const postsTable = pgTable('posts_table', {
-  id: serial('id').primaryKey(),
-  title: text('title').notNull(),
-  content: text('content').notNull(),
-  userId: integer('user_id')
-    .notNull()
-    .references(() => usersTable.id, { onDelete: 'cascade' }),
-  createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at')
-    .notNull()
-    .$onUpdate(() => new Date()),
-});
+export const foldersTable = pgTable(
+  "folders",
+  {
+    id: serial("id").primaryKey().notNull(),
+    name: text("name").notNull(),
+    parentId: integer("parentId"),
+    createdAt: timestamp("createdAt").defaultNow().notNull(),
+    updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+  },
+  (table) => {
+    return {
+      indexes: [index("parentIdIndex").on(table.parentId)],
+    };
+  },
+);
 
-export type InsertUser = typeof usersTable.$inferInsert;
-export type SelectUser = typeof usersTable.$inferSelect;
+export type InsertFile = typeof filesTable.$inferInsert;
+export type SelectFile = typeof filesTable.$inferSelect;
 
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
+export type InsertFolder = typeof foldersTable.$inferInsert;
+export type SelectFolder = typeof foldersTable.$inferSelect;
