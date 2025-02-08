9eaa0973112879cb2a22048f674129f75ae24d88
Owen Goh <owengoh2001@gmail.com>
2025-02-08 14:19:18 +0800

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,21 @@
 import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Google Drive Clone",
+  description:
+    "A Google Drive Clone built with Next.js, Drizzle, Neon, and Clerk.",
+  icons: [
+    {
+      rel: "icon",
+      url: "/favicon.ico",
+    },
+  ],
+};
 
 export default function RootLayout({
   children,
